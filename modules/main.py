64642b4daadc3f1a832c3e24c1baf78d21f57d0c
creator154 <cteator898@gmail.com>
2026-03-25 19:12:26 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -311,7 +311,7 @@ async def txt_handler(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**🏷️ Iɴᴅᴇx ID: {str(count).zfill(3)}.\n\n🎞️Tɪᴛʟᴇ: {name1} {res}.mkv\n\n<pre><code>📚 𝗕ᴀᴛᴄʜ: {b_name}</code></pre>\n\n📥 Uᴘʟᴏᴀᴅ Bʏ :\n【 \n\n {CR}\n\n】\n\n**━━━━━✦𝗭𝗫✦━━━━━**'
+                cc = f'**🏷️ Iɴᴅᴇx ID: {str(count).zfill(3)}.\n\n🎞️Tɪᴛʟᴇ: {name1} {res}.mkv\n\n<pre><code>📚 𝗕ᴀᴛᴄʜ: {b_name}</code></pre>\n\n📥 <b>Uᴘʟᴏᴀᴅ Bʏ :</b> 【 <a href='https://t.me/" + CR.replace('@','') + "'>" + CR + "</a> 】\n\n**━━━━━✦𝗭𝗫✦━━━━━**'
                 cc1 = f'**🏷️ Iɴᴅᴇx ID: {str(count).zfill(3)}.\n\n🎞️Tɪᴛʟᴇ: {name1} .pdf\n\n<pre><code>📚 𝗕ᴀᴛᴄʜ: {b_name}</code></pre>\n\n📥 Uᴘʟᴏᴀᴅ Bʏ :\n【 \n\n {CR}\n\n】\n\n**∘━━━━━✦𝗭𝗫✦━━━━━**'
                     
                 
